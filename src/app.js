fa62e523852ece5e0636df4186d06c40b54f40b2
Meghna Das <gdyes128@gmail.com>
2023-10-30 12:23:10 +0530

    changes made

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import './app.css';
+import Dateofbirth from './components/Dateofbirth';
+
+function App() {
+  return (
+    <div className="App">
+      <Dateofbirth />
+    </div>
+  );
+}
+
+export default App;
